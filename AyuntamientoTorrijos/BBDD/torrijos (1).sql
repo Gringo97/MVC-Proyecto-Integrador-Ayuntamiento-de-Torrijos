c0393b40f459f5c035b0f4aae8e4a8f139e4c3f6
Martin <Martin@192.168.1.17>
2017-06-02 05:30:44 +0200

    Introduccion Datos

--- AyuntamientoTorrijos/BBDD/torrijos (1).sql
+++ AyuntamientoTorrijos/BBDD/torrijos (1).sql
@@ -1,9 +1,9 @@
-﻿-- phpMyAdmin SQL Dump
+-- phpMyAdmin SQL Dump
 -- version 4.7.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 02-06-2017 a las 01:40:42
+-- Tiempo de generación: 02-06-2017 a las 05:28:38
 -- Versión del servidor: 10.1.22-MariaDB
 -- Versión de PHP: 7.1.4
 
@@ -30,8 +30,9 @@ USE `torrijos`;
 -- Estructura de tabla para la tabla `actividad`
 --
 
-CREATE TABLE IF NOT EXISTS `actividad` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `actividad`;
+CREATE TABLE `actividad` (
+  `id` int(11) NOT NULL,
   `fotoLicenciaObra` tinyint(11) DEFAULT NULL,
   `fotoOtrasAutorizaciones` tinyint(11) DEFAULT NULL,
   `fotoJustificantePago` tinyint(11) DEFAULT NULL,
@@ -54,9 +55,8 @@ CREATE TABLE IF NOT EXISTS `actividad` (
   `certModelo1` tinyint(11) DEFAULT NULL,
   `certModelo2` tinyint(11) DEFAULT NULL,
   `FotocopiaDNI` tinyint(1) DEFAULT NULL,
-  `Estado` enum('Procesando','Aceptada','Archivada','') NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+  `Estado` enum('Procesando','Aceptada','Archivada','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `actividad`
@@ -70,7 +70,10 @@ INSERT INTO `actividad` (`id`, `fotoLicenciaObra`, `fotoOtrasAutorizaciones`, `f
 (23, 1, 0, 0, 1, 0, 1, 0, 0, 0, 'urbano', 'SI', 'LENCERIAS JUDITH', 'COMERCIO', 'NO', '14/06/2017', '03/06/2017', 100, 'ASDF', 0, 1, 0, 0, 'Procesando'),
 (24, 1, 1, 0, 1, 0, 1, 0, 1, 0, 'urbano', 'SI', 'FRUTERIAS FERNANDEZ', 'COMERCIO', 'NO', '17/06/2017', '06/06/2017', 100, 'ASDF', 0, 0, 1, 1, 'Aceptada'),
 (25, 1, 0, 0, 0, 0, 0, 0, 0, 1, 'urbano', 'SI', 'RECINTO FERIAL', 'OCIO', 'NO', '16/06/2017', '08/06/2017', 100, 'ASDF', 0, 0, 0, 0, 'Aceptada'),
-(26, 1, 0, 1, 1, 1, 1, 1, 1, 1, 'urbano', 'NO', 'CATERING RAM', 'COMERCIO', 'NO', '22/06/2017', '06/06/2017', 1000, 'ASDF', 0, 1, 1, 1, 'Aceptada');
+(26, 1, 0, 1, 1, 1, 1, 1, 1, 1, 'urbano', 'NO', 'CATERING RAM', 'COMERCIO', 'NO', '22/06/2017', '06/06/2017', 1000, 'ASDF', 0, 1, 1, 1, 'Aceptada'),
+(27, 1, 0, 0, 0, 0, 1, 0, 0, 1, 'rustico', 'SI¡¡nno', 'Ciber', 'BAR CIBER', 'SI', '03/06/2017', '01/06/2017', 5000, 'PRUEBA REALIZADA', 0, 1, 1, 0, 'Aceptada'),
+(28, 1, 0, 1, 0, 0, 0, 1, 0, 0, 'rustico', 'SI', 'BAR', 'CIBER', 'SI', '16/06/2017', '01/06/2017', 50, 'PRUEBA REALIZADA', 0, 0, 0, 1, 'Archivada'),
+(29, 1, 1, 1, 0, 0, 1, 0, 0, 0, 'urbano', 'SI', 'BAR', 'COMICO', 'NO', '17/06/2017', '14/06/2017', 500, 'PRUEBA SOLICITUD', 0, 1, 0, 0, 'Procesando');
 
 -- --------------------------------------------------------
 
@@ -78,17 +81,16 @@ INSERT INTO `actividad` (`id`, `fotoLicenciaObra`, `fotoOtrasAutorizaciones`, `f
 -- Estructura de tabla para la tabla `cambiotitularidad`
 --
 
-CREATE TABLE IF NOT EXISTS `cambiotitularidad` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `cambiotitularidad`;
+CREATE TABLE `cambiotitularidad` (
+  `id` int(11) NOT NULL,
   `idLicenciaAnterior` int(11) NOT NULL,
   `numExpediente` tinyint(4) DEFAULT NULL,
   `escrituraPropiedad` tinyint(4) DEFAULT NULL,
   `contratoArrendamiento` tinyint(4) DEFAULT NULL,
   `documentacionJust` tinyint(4) DEFAULT NULL,
   `planosYPlanta` tinyint(4) DEFAULT NULL,
-  `fotoLicenciaAnterior` tinyint(4) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idLicenciaAnterior` (`idLicenciaAnterior`)
+  `fotoLicenciaAnterior` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -97,8 +99,9 @@ CREATE TABLE IF NOT EXISTS `cambiotitularidad` (
 -- Estructura de tabla para la tabla `interesado`
 --
 
-CREATE TABLE IF NOT EXISTS `interesado` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `interesado`;
+CREATE TABLE `interesado` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellidos` varchar(100) NOT NULL,
   `cif` varchar(50) NOT NULL,
@@ -108,9 +111,8 @@ CREATE TABLE IF NOT EXISTS `interesado` (
   `telefonoFijo` int(12) DEFAULT NULL,
   `telefonoMovil` int(12) NOT NULL,
   `fax` int(12) DEFAULT NULL,
-  `email` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=latin1;
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `interesado`
@@ -118,13 +120,16 @@ CREATE TABLE IF NOT EXISTS `interesado` (
 
 INSERT INTO `interesado` (`id`, `nombre`, `apellidos`, `cif`, `direccion`, `municipio`, `codigoPostal`, `telefonoFijo`, `telefonoMovil`, `fax`, `email`) VALUES
 (24, 'JUAN', 'LUIS', '12345678O', 'C/ lOS OLIVOS 2', 'SEGOVIA', 24567, 918437362, 918437362, 918437362, 'juanluis@gmail.com'),
-(25, 'GUILLERO', 'TUVILLA CALVO', '12345674K', 'C/ ENFERMO 2', 'MORALZARZAL', 28411, 918571827, 918571827, 918571827, 'shotual@gmail.com'),
+(25, 'GUILLERO', 'TUVILLA Dios', '12345674K', 'C/ ENFERMO 2', 'MORALZARZAL', 28411, 918571827, 918571827, 918571827, 'shotual@gmail.com'),
 (31, 'JUAN', 'RAMON JIMENEZ', '91827483J', 'CALLE BAJA 5', 'SEGOVIA', 23543, 912849374, 912849374, 912849374, 'JUANMA@GMAIL.COM'),
 (32, 'PEDRO', 'CALVO RAMOS', '574829482J', 'CALLE ALTA 3', 'TORDESILLAS', 23543, 912849374, 912849374, 912849374, 'PEDRO@GMAIL.COM'),
 (33, 'JUDITH', 'ARRANZ SOBRINO', '43423554J', 'CALLE POSTUREO 1', 'MORALZARZAL', 28411, 912849374, 912849374, 912849374, 'JUDITH@GMAIL.COM'),
 (34, 'JAVIER', 'FERNANDEZ TRISTE', '56438202J', 'CALLE LARGA 2', 'LAS ROZAS ', 23400, 912849374, 912849374, 912849374, 'JAVIER@TRISTE.COM'),
 (35, 'SERGIO', 'REDONDO MONTES', '47568382J', 'CALLE ESTRECHA 3', 'MADRID', 23400, 912849374, 912849374, 912849374, 'SERGIO@REDONDO.COM'),
-(36, 'RAMSES', 'DIEZ CAYETANO', '7534648K', 'CALLE CUESTA ARRIBA 4', 'HOYO DE MANZANARES', 25432, 912849374, 912849374, 912849374, 'RAM@GMAIL.COM');
+(36, 'RAMSES', 'DIEZ CAYETANO', '7534648K', 'CALLE CUESTA ARRIBA 4', 'HOYO DE MANZANARES', 25432, 912849374, 912849374, 912849374, 'RAM@GMAIL.COM'),
+(37, 'David', 'Perez', '54089658P', 'C/ LAS ROZAS', 'LAS ROZAS', 28411, 9158647, 61999587, 9184597, 'PEDRO@GMAIL.COM'),
+(38, 'Jaime', 'de la torre', '5879655P', 'LAS ROZAS', 'LAS ROZAS', 28411, 61991415, 61991416, 91589696, 'PEDRO@GMAIL.COM'),
+(39, 'Lafoz', 'GOMEZ', '548548Y', 'LAS ROZAS', 'LAS ROZAS', 28411, 6188787, 61999999, 9185588, 'ESTHER@GMAIL.COM');
 
 -- --------------------------------------------------------
 
@@ -132,14 +137,12 @@ INSERT INTO `interesado` (`id`, `nombre`, `apellidos`, `cif`, `direccion`, `muni
 -- Estructura de tabla para la tabla `relactper`
 --
 
-CREATE TABLE IF NOT EXISTS `relactper` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `relactper`;
+CREATE TABLE `relactper` (
+  `id` int(11) NOT NULL,
   `idInt` int(11) NOT NULL,
-  `idAct` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idInt` (`idInt`),
-  KEY `idAct` (`idAct`)
-) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=latin1;
+  `idAct` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `relactper`
@@ -153,7 +156,10 @@ INSERT INTO `relactper` (`id`, `idInt`, `idAct`) VALUES
 (23, 33, 23),
 (24, 34, 24),
 (25, 35, 25),
-(26, 36, 26);
+(26, 36, 26),
+(27, 37, 27),
+(28, 38, 28),
+(29, 39, 29);
 
 -- --------------------------------------------------------
 
@@ -161,14 +167,12 @@ INSERT INTO `relactper` (`id`, `idInt`, `idAct`) VALUES
 -- Estructura de tabla para la tabla `relintrep`
 --
 
-CREATE TABLE IF NOT EXISTS `relintrep` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `relintrep`;
+CREATE TABLE `relintrep` (
+  `id` int(11) NOT NULL,
   `idInteresado` int(11) NOT NULL,
-  `idRepresentante` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idInteresado` (`idInteresado`,`idRepresentante`),
-  KEY `idRepresentante` (`idRepresentante`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+  `idRepresentante` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `relintrep`
@@ -184,8 +188,9 @@ INSERT INTO `relintrep` (`id`, `idInteresado`, `idRepresentante`) VALUES
 -- Estructura de tabla para la tabla `representante`
 --
 
-CREATE TABLE IF NOT EXISTS `representante` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `representante`;
+CREATE TABLE `representante` (
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellidos` varchar(100) NOT NULL,
   `documentoIdentidad` varchar(9) NOT NULL,
@@ -195,9 +200,8 @@ CREATE TABLE IF NOT EXISTS `representante` (
   `telefonoFijo` int(12) DEFAULT NULL,
   `telefonoMovil` int(12) NOT NULL,
   `fax` int(12) DEFAULT NULL,
-  `Email` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+  `Email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `representante`
@@ -207,6 +211,85 @@ INSERT INTO `representante` (`id`, `nombre`, `apellidos`, `documentoIdentidad`, 
 (8, 'JUAN', 'LUIS', '12345678O', 'C/ lOS OLIVOS 2', 'SEGOVIA', 24567, 918437362, 918437362, 918437362, 'juanluis@gmail.com'),
 (9, 'GUILLERO', 'TUVILLA CALVO', '12345674K', 'C/ ENFERMO 2', 'MORALZARZAL', 28411, 918571827, 918571827, 918571827, 'shotual@gmail.com');
 
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `actividad`
+--
+ALTER TABLE `actividad`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `cambiotitularidad`
+--
+ALTER TABLE `cambiotitularidad`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idLicenciaAnterior` (`idLicenciaAnterior`);
+
+--
+-- Indices de la tabla `interesado`
+--
+ALTER TABLE `interesado`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `relactper`
+--
+ALTER TABLE `relactper`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idInt` (`idInt`),
+  ADD KEY `idAct` (`idAct`);
+
+--
+-- Indices de la tabla `relintrep`
+--
+ALTER TABLE `relintrep`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idInteresado` (`idInteresado`,`idRepresentante`),
+  ADD KEY `idRepresentante` (`idRepresentante`);
+
+--
+-- Indices de la tabla `representante`
+--
+ALTER TABLE `representante`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `actividad`
+--
+ALTER TABLE `actividad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+--
+-- AUTO_INCREMENT de la tabla `cambiotitularidad`
+--
+ALTER TABLE `cambiotitularidad`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `interesado`
+--
+ALTER TABLE `interesado`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
+--
+-- AUTO_INCREMENT de la tabla `relactper`
+--
+ALTER TABLE `relactper`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+--
+-- AUTO_INCREMENT de la tabla `relintrep`
+--
+ALTER TABLE `relintrep`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+--
+-- AUTO_INCREMENT de la tabla `representante`
+--
+ALTER TABLE `representante`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- Restricciones para tablas volcadas
 --
